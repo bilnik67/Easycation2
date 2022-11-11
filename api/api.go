3d5048560613df100e558820724a96465c44b7c3
maya <mayafransen2003@gmail.com>
2022-11-11 08:41:11 +0100

    started working on database connections and transfers

--- api/api.go
+++ api/api.go
@@ -1,9 +1,11 @@
 package api
 
 import (
+	"database/sql"
 	"fmt"
 	_ "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 	"net/http"
 	"strconv"
 )
@@ -14,6 +16,8 @@ func HandleRequests() {
 	router.HandleFunc("/", HandleFuncStarter).Methods("GET")
 
 	router.Handle("/book/{id}", BookDownloader).Methods("GET")
+
+	router.Handle("/register/start/{fName, lName, email, password}", Registerer).Methods("POST")
 
 	http.ListenAndServe(":"+strconv.Itoa(6969), router)
 }
@@ -28,6 +32,15 @@ var BookDownloader = http.HandlerFunc(func(w http.ResponseWriter, r *http.Reques
 	return
 })
 
+var Registerer = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	PostRegister(w, r)
+	return
+})
+
 func GetBooks(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/book/{id}", http.StatusSeeOther)
+}
+
+func PostRegister(db *sql.DB, r *http.Request) {
+
 }
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "awesomeProject1/api"
+import (
+	"awesomeProject1/api"
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
 
 func main() {
 	api.HandleRequests()
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", "db", 5555, "root", "root", "EasyCation_DB")
+
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+
+	// close database
+	defer db.Close()
+
+	// check db
+	err = db.Ping()
+	CheckError(err)
+
+	fmt.Println("Connected!")
+}
+
+func CheckError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
